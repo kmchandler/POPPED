080430a57becefc8a8d9f4bf3a04192574d362d8
Kristen Chandler <krismchandler@gmail.com>
2022-08-22 18:49:25 -0500

    got new user form to render and the button to properly route

---
+++ pages/users/edit/[userFirebaseKey].js
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
+import { getSingleUser } from '../../../api/userData';
+import CreateUserForm from '../new';
+
+export default function EditMembers() {
+  const [editItem, setEditItem] = useState({});
+  const router = useRouter();
+  const { userFirebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleUser(userFirebaseKey).then(setEditItem);
+  }, [userFirebaseKey]);
+
+  return (<CreateUserForm obj={editItem} />);
+}
