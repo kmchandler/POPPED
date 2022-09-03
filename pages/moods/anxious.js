dc9fe3a699fdd8cac7bc876869fffa0790c508a0
Kristen Chandler <krismchandler@gmail.com>
2022-09-03 13:17:42 -0500

    got the anxious mood page to render correctly

--- pages/moods/anxious.js
+++ pages/moods/anxious.js
@@ -1,28 +1,35 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../utils/context/authContext';
 import FlickCard from '../../components/FlickCard';
 import { getFlicksByUidWithMetaData } from '../../api/mergedData';
 
-export default function Watchlist() {
+export default function Anxious() {
   const [flicks, setFlicks] = useState([]);
   const { user } = useAuth();
 
   const getAnxiousFlicks = async () => {
     const flicksWithMetaData = await getFlicksByUidWithMetaData(user.uid);
-    console.warn(flicksWithMetaData);
-    const flicksByMood = flicksWithMetaData.filter((flk) => flk.moods.moodsName.includes('anxios'));
-    setFlicks(flicksByMood);
+    const flickMoods = [];
+    flicksWithMetaData.map((md) => md.moods.filter((mood) => {
+      if (mood.moodsName.includes('Anxious')) {
+        console.warn(md);
+        flickMoods.push(md);
+      }
+    }));
+    return flickMoods;
   };
 
   useEffect(() => {
-    getAnxiousFlicks();
+    getAnxiousFlicks().then(setFlicks);
   }, [user]);
 
   return (
     <>
       <div className="d-flex flex-wrap cardContainer">
-        {flicks.map((flix) => <FlickCard key={flix.flicksFirebaseKey} flickObj={flix} onUpdate={getAnxiousFlicks} />)}
+        {flicks?.map((flix) => <FlickCard key={flix.flicksFirebaseKey} flickObj={flix} onUpdate={getAnxiousFlicks} />)}
       </div>
     </>
   );
