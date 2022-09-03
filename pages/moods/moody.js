5e4facfc0a83af481eb7a57da62561a34675ec27
Kristen Chandler <krismchandler@gmail.com>
2022-09-03 13:48:22 -0500

    got all moods pages rendering properly

--- pages/moods/moody.js
+++ pages/moods/moody.js
@@ -1,7 +1,47 @@
-import React from 'react';
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../utils/context/authContext';
+import FlickCard from '../../components/FlickCard';
+import { getFlicksByUidWithMetaData } from '../../api/mergedData';
 
-export default function moody() {
-  return (
-    <div>moody</div>
+export default function Moody() {
+  const [flicks, setFlicks] = useState([]);
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const getMoodyFlicks = async () => {
+    const flicksWithMetaData = await getFlicksByUidWithMetaData(user.uid);
+    const flickMoods = [];
+    flicksWithMetaData.map((md) => md.moods.filter((mood) => {
+      if (mood.moodsName.includes('Moody')) {
+        flickMoods.push(md);
+      }
+    }));
+    return flickMoods;
+  };
+
+  useEffect(() => {
+    getMoodyFlicks().then(setFlicks);
+  }, [user]);
+
+  const onClick = () => router.push('/moods/moods');
+
+  if (flicks.length <= 0) {
+    return (
+      <>
+        <h3>No flicks found matching this mood.</h3>
+        <button type="button" onClick={onClick} className="backToMoodsBtn">previous page</button>
+      </>
+    );
+  } return (
+    <>
+      <button type="button" onClick={onClick} className="backToMoodsBtn">previous page</button>
+      <div className="d-flex flex-wrap cardContainer">
+        {flicks?.map((flix) => <FlickCard key={flix.flicksFirebaseKey} flickObj={flix} onUpdate={getMoodyFlicks} />)}
+      </div>
+    </>
   );
 }
