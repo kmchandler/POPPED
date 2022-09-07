4bb6309923bd06d2e211323f4a9648c9c11c85d9
Kristen Chandler <krismchandler@gmail.com>
2022-09-07 15:09:55 -0500

    logic to render fav flicks to user page

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,15 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useAuth } from '../utils/context/authContext';
+import { getFlicksByUidWithMetaData } from '../api/mergedData';
 
 export default function ProfileCard({ userObj }) {
+  const [flicks, setFlicks] = useState([]);
+  const { user } = useAuth();
+
+  const getAllTheFlicks = async () => {
+    const flicksWithMetaData = await getFlicksByUidWithMetaData(user.uid);
+    setFlicks(flicksWithMetaData);
+  };
+
+  useEffect(() => {
+    getAllTheFlicks();
+  }, [user]);
+
   return (
     <div>
       <img src={userObj.imageUrl} alt={userObj.username} />
       <h1>{userObj.firstName} {userObj.lastName}</h1>
       <h2>{userObj.username}</h2>
-      <h3>Faorite Genres:</h3>
+      <h3>Favorite Genres:</h3>
       {userObj.genres?.map((genre) => <h3>{genre.genreName}</h3>)}
+      <h3>Favorite Flicks:</h3>
+      {flicks?.map((flick) => {
+        if (!flick.favorite) return null;
+
+        return <h3>{flick.flickName}</h3>;
+      })}
     </div>
   );
 }
@@ -23,7 +44,11 @@ ProfileCard.propTypes = {
     imageUrl: PropTypes.string,
     userFirebaseKey: PropTypes.string,
   }),
+  user: PropTypes.shape({
+    uid: PropTypes.string,
+  }).isRequired,
 };
+
 ProfileCard.defaultProps = {
   userObj: {},
 };
